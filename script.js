26b80fb881d2b3ec9c52f6466810964a61d8d1ad
Anderson Ryan <santoryan1408@gmail.com>
2022-04-04 18:49:48 -0300

    base design added

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-const mobileMenu = document.getElementById('mobile-menu')
 const mobileButton = document.getElementById('mobile-button');
+const mobileMenu = document.getElementById('mobile-menu');
+const navBar = document.getElementById('navbar');
+const logo = navBar.children[0].children[0];
+const navLists = navBar.children[2].children;
 let isOpen = false;
 
 mobileButton.addEventListener('click', showMenu);
+window.addEventListener('scroll', changeNavColor);
 
 function showMenu(){
-
-    if (window.matchMedia("(min-width: 1100px)").matches){
-        const isOpen = false;
-    }else if(!isOpen){
+    if(!isOpen){
         mobileMenu.style.display = 'flex';
+        mobileButton.style.color = 'white'
+        logo.style.color = 'white';
         isOpen = true;
     }else if(isOpen){
         mobileMenu.style.display = 'none';
+        mobileButton.style.color = 'rgb(243, 138, 0)';
+        logo.style.color = 'rgb(243, 138, 0)'
         isOpen = false;
     }
+}
+
+function changeNavColor(){
+    let posy = window.pageYOffset;
+    let buttonColor = posy >=550? 'rgb(243, 138, 0)': 'white';
+    let color = posy >= 630? 'black': 'white';
+
+    mobileButton.style.color = buttonColor;
+    navLists[0].children[0].style.color = color;
+    navLists[1].children[0].style.color = color;
+    navLists[2].children[0].style.color = color;
+    navLists[3].children[0].style.color = color;
 }
